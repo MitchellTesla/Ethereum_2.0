a7e976bced04af174e151069025202aa20eb651d
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-11 23:48:53 -0400

    Update

---
+++ Integrations/fe/compiler/src/lowering/mod.rs
@@ -0,0 +1,13 @@
+//! Fe Lowering.
+
+use crate::types::FeModuleAst;
+use fe_analyzer::context::Context;
+
+mod mappers;
+mod names;
+mod utils;
+
+/// Lowers the Fe source AST to a Fe HIR AST.
+pub fn lower(context: &Context, module: FeModuleAst) -> FeModuleAst {
+    mappers::module::module(context, module)
+}
