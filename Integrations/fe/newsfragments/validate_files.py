92154ad483e526f2d38e422da65eba1fca9bca09
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-11 23:39:10 -0400

    Update

---
+++ Integrations/fe/newsfragments/validate_files.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Towncrier silently ignores files that do not match the expected ending.
+# We use this script to ensure we catch these as errors in CI.
+
+import os
+import pathlib
+import sys
+
+ALLOWED_EXTENSIONS = {
+    '.bugfix.md',
+    '.doc.md',
+    '.feature.md',
+    '.internal.md',
+    '.misc.md',
+    '.performance.md',
+    '.removal.md',
+}
+
+ALLOWED_FILES = {
+    'validate_files.py',
+    'README.md',
+}
+
+THIS_DIR = pathlib.Path(__file__).parent
+
+num_args = len(sys.argv) - 1
+assert num_args in {0, 1}
+if num_args == 1:
+    assert sys.argv[1] in ('is-empty', )
+
+def ends_with_newline(file):
+    with open(file, 'r') as file:
+        return file.read().endswith('\n')
+
+for fragment_file in THIS_DIR.iterdir():
+    if fragment_file.name in ALLOWED_FILES:
+        continue
+    elif num_args == 0:
+        full_extension = "".join(fragment_file.suffixes)
+        if full_extension not in ALLOWED_EXTENSIONS:
+            raise Exception(f"Unexpected file: {fragment_file}")
+        elif not ends_with_newline(fragment_file):
+            raise Exception(
+                    f"Fragment files need to end with new line but { fragment_file.name } does not."
+                )
+    elif sys.argv[1] == 'is-empty':
+        raise Exception(f"Unexpected file: {fragment_file}")
+    else:
+        raise RuntimeError("Strange: arguments {sys.argv} were validated, but not found")
