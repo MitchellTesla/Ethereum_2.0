5bd844d3fbbfe4a4448778ededef054d055d5327
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-11 23:49:57 -0400

    Update

---
+++ Integrations/fe/compiler/src/abi/utils.rs
@@ -0,0 +1,17 @@
+use fe_common::utils::keccak;
+
+/// Formats the name and fields and calculates the 32 byte keccak256 value of
+/// the signature.
+pub fn event_topic(name: &str, fields: Vec<String>) -> String {
+    sign_event_or_func(name, fields, 32)
+}
+/// Formats the name and params and calculates the 4 byte keccak256 value of the
+/// signature.
+pub fn func_selector(name: &str, params: Vec<String>) -> String {
+    sign_event_or_func(name, params, 4)
+}
+
+fn sign_event_or_func(name: &str, params: Vec<String>, size: usize) -> String {
+    let signature = format!("{}({})", name, params.join(","));
+    keccak::partial(signature.as_bytes(), size)
+}
