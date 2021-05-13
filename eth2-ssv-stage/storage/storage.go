2fe7e4586f6442ed555aaea26cb86a3593696803
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 02:12:20 -0400

    Update eth2-ssv-stage/storage

---
+++ eth2-ssv-stage/storage/storage.go
@@ -0,0 +1,16 @@
+package storage
+
+// IKvStorage interface for all db kind
+type IKvStorage interface {
+	Set(prefix []byte, key []byte, value []byte) error
+	Get(prefix []byte, key []byte) (Obj, error)
+	GetAllByCollection(prefix []byte) ([]Obj, error)
+}
+
+// Obj struct for getting key/value from storage
+type Obj struct {
+	Key []byte
+	Value []byte
+}
+
+
