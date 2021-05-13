a0a5020348835f8a8851d82071b0e8a6308523aa
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 02:27:33 -0400

    Update eth2-ssv-stage/ibft/proto

---
+++ eth2-ssv-stage/ibft/proto/params_test.go
@@ -0,0 +1,69 @@
+package proto
+
+import (
+	"testing"
+
+	"github.com/herumi/bls-eth-go-binary/bls"
+	"github.com/stretchr/testify/require"
+)
+
+func generateNodes(cnt int) (map[uint64]*bls.SecretKey, map[uint64]*Node) {
+	bls.Init(bls.BLS12_381)
+	nodes := make(map[uint64]*Node)
+	sks := make(map[uint64]*bls.SecretKey)
+	for i := 0; i < cnt; i++ {
+		sk := &bls.SecretKey{}
+		sk.SetByCSPRNG()
+
+		nodes[uint64(i)] = &Node{
+			IbftId: uint64(i),
+			Pk:     sk.GetPublicKey().Serialize(),
+		}
+		sks[uint64(i)] = sk
+	}
+	return sks, nodes
+}
+
+func signMsg(id uint64, secretKey *bls.SecretKey, msg *Message) *SignedMessage {
+	signature, _ := msg.Sign(secretKey)
+	return &SignedMessage{
+		Message:   msg,
+		Signature: signature.Serialize(),
+		SignerIds: []uint64{id},
+	}
+}
+
+func TestInstanceParams_ThresholdSize(t *testing.T) {
+	for i := 1; i < 50; i++ {
+		p := &InstanceParams{IbftCommittee: make(map[uint64]*Node)}
+		// populate
+		for j := 1; j <= 3*i+1; j++ {
+			p.IbftCommittee[uint64(j)] = &Node{}
+		}
+		require.EqualValues(t, 2*i+1, p.ThresholdSize())
+	}
+}
+
+func TestPubKeysById(t *testing.T) {
+	secretKeys, nodes := generateNodes(4)
+	params := &InstanceParams{
+		IbftCommittee: nodes,
+	}
+
+	// test single
+	pks, err := params.PubKeysByID([]uint64{0})
+	require.NoError(t, err)
+	require.Len(t, pks, 1)
+	require.EqualValues(t, pks[0].Serialize(), secretKeys[0].GetPublicKey().Serialize())
+
+	// test multiple
+	pks, err = params.PubKeysByID([]uint64{0, 1})
+	require.NoError(t, err)
+	require.Len(t, pks, 2)
+	require.EqualValues(t, pks[0].Serialize(), secretKeys[0].GetPublicKey().Serialize())
+	require.EqualValues(t, pks[1].Serialize(), secretKeys[1].GetPublicKey().Serialize())
+
+	// test multiple with invalid
+	_, err = params.PubKeysByID([]uint64{0, 5})
+	require.EqualError(t, err, "pk for id not found")
+}
