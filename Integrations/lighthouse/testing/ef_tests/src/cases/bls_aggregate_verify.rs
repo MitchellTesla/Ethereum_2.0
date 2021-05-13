683202c99a0d6bf81b5e612b5c84c8b439ac52fc
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 13:06:18 -0400

    Update lighthouse eftests src cases

---
+++ Integrations/lighthouse/testing/ef_tests/src/cases/bls_aggregate_verify.rs
@@ -0,0 +1,63 @@
+use super::*;
+use crate::case_result::compare_result;
+use crate::cases::common::BlsCase;
+use bls::{AggregateSignature, PublicKeyBytes};
+use serde_derive::Deserialize;
+use types::Hash256;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct BlsAggregateVerifyInput {
+    pub pubkeys: Vec<PublicKeyBytes>,
+    pub messages: Vec<String>,
+    pub signature: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct BlsAggregateVerify {
+    pub input: BlsAggregateVerifyInput,
+    pub output: bool,
+}
+
+impl BlsCase for BlsAggregateVerify {}
+
+impl Case for BlsAggregateVerify {
+    fn result(&self, _case_index: usize) -> Result<(), Error> {
+        let messages = self
+            .input
+            .messages
+            .iter()
+            .map(|message| {
+                let bytes = hex::decode(&message[2..])
+                    .map_err(|e| Error::FailedToParseTest(format!("{:?}", e)))?;
+                Ok(Hash256::from_slice(&bytes))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        let pubkeys_result = self
+            .input
+            .pubkeys
+            .iter()
+            .map(|pkb| pkb.decompress())
+            .collect::<Result<Vec<_>, _>>();
+
+        let pubkeys = match pubkeys_result {
+            Ok(pubkeys) => pubkeys,
+            Err(bls::Error::InvalidInfinityPublicKey) if !self.output => {
+                return Ok(());
+            }
+            Err(e) => return Err(Error::FailedToParseTest(format!("{:?}", e))),
+        };
+
+        let pubkey_refs = pubkeys.iter().collect::<Vec<_>>();
+
+        let signature_bytes = hex::decode(&self.input.signature[2..])
+            .map_err(|e| Error::FailedToParseTest(format!("{:?}", e)))?;
+
+        let signature_valid = AggregateSignature::deserialize(&signature_bytes)
+            .ok()
+            .map(|signature| signature.aggregate_verify(&messages, &pubkey_refs[..]))
+            .unwrap_or(false);
+
+        compare_result::<bool, ()>(&Ok(signature_valid), &Some(self.output))
+    }
+}
---
+++ Integrations/lighthouse/testing/ef_tests/src/cases/bls_verify_msg.rs
@@ -0,0 +1,39 @@
+use super::*;
+use crate::case_result::compare_result;
+use crate::cases::common::BlsCase;
+use bls::{PublicKeyBytes, Signature, SignatureBytes};
+use serde_derive::Deserialize;
+use std::convert::TryInto;
+use types::Hash256;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct BlsVerifyInput {
+    pub pubkey: PublicKeyBytes,
+    pub message: String,
+    pub signature: SignatureBytes,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct BlsVerify {
+    pub input: BlsVerifyInput,
+    pub output: bool,
+}
+
+impl BlsCase for BlsVerify {}
+
+impl Case for BlsVerify {
+    fn result(&self, _case_index: usize) -> Result<(), Error> {
+        let message = hex::decode(&self.input.message[2..])
+            .map_err(|e| Error::FailedToParseTest(format!("{:?}", e)))?;
+
+        let signature_ok = (&self.input.signature)
+            .try_into()
+            .and_then(|signature: Signature| {
+                let pk = self.input.pubkey.decompress()?;
+                Ok(signature.verify(&pk, Hash256::from_slice(&message)))
+            })
+            .unwrap_or(false);
+
+        compare_result::<bool, ()>(&Ok(signature_ok), &Some(self.output))
+    }
+}
