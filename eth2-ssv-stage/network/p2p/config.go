2093411b643cbc3e670b3b294253ed20f81c83b8
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 02:15:53 -0400

    Update eth2-ssv-stage/network/p2p

---
+++ eth2-ssv-stage/network/p2p/config.go
@@ -0,0 +1,19 @@
+package p2p
+
+import (
+	"github.com/libp2p/go-libp2p-core/peer"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+)
+// Config - describe the config options for p2p network
+type Config struct {
+	DiscoveryType       string
+	BootstrapNodeAddr   []string
+	Discv5BootStrapAddr []string
+	UDPPort             int
+	TCPPort             int
+	HostAddress         string
+	HostDNS             string
+	HostID              peer.ID
+	Topics map[string]*pubsub.Topic
+	Subs   []*pubsub.Subscription
+}
