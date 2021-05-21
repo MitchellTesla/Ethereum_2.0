9f2d3f11a2b4ec32a793440d1154d431e7f7ee4c
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-21 17:43:11 -0500

    Add ZeroKnowledge/zokrates_core/tests

---
+++ ZeroKnowledge/zokrates_core/tests/integration.rs
@@ -0,0 +1,16 @@
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate zokrates_core;
+extern crate zokrates_field;
+
+#[macro_use]
+mod utils;
+
+zokrates_test! {
+    add,
+    assert_one,
+    array_if,
+    fact_up_to_4,
+    split,
+}
