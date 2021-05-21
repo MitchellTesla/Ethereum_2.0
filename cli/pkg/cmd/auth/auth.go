be2967e1819e40e2d6faf89eeb47682f5f33c46b
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-21 06:41:46 -0500

    update cli pkg cmd auth

---
+++ cli/pkg/cmd/auth/auth.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	gitCredentialCmd "github.com/cli/cli/pkg/cmd/auth/gitcredential"
+	authLoginCmd "github.com/cli/cli/pkg/cmd/auth/login"
+	authLogoutCmd "github.com/cli/cli/pkg/cmd/auth/logout"
+	authRefreshCmd "github.com/cli/cli/pkg/cmd/auth/refresh"
+	authStatusCmd "github.com/cli/cli/pkg/cmd/auth/status"
+	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdAuth(f *cmdutil.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "auth <command>",
+		Short: "Login, logout, and refresh your authentication",
+		Long:  `Manage gh's authentication state.`,
+	}
+
+	cmdutil.DisableAuthCheck(cmd)
+
+	cmd.AddCommand(authLoginCmd.NewCmdLogin(f, nil))
+	cmd.AddCommand(authLogoutCmd.NewCmdLogout(f, nil))
+	cmd.AddCommand(authStatusCmd.NewCmdStatus(f, nil))
+	cmd.AddCommand(authRefreshCmd.NewCmdRefresh(f, nil))
+	cmd.AddCommand(gitCredentialCmd.NewCmdCredential(f, nil))
+
+	return cmd
+}
