7e7257b4cf86ed5340cc795c452b5d9dd6477ad7
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-22 20:11:39 -0400

    update consul-terraform-sync/templates/tftmpl

---
+++ consul-terraform-sync/templates/tftmpl/golden_test.go
@@ -0,0 +1,190 @@
+package tftmpl
+
+import (
+	"bytes"
+	"flag"
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/hashicorp/consul-terraform-sync/templates/hcltmpl"
+	goVersion "github.com/hashicorp/go-version"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/zclconf/go-cty/cty"
+)
+
+var update = flag.Bool("update", false, "update golden files")
+
+func TestNewFiles(t *testing.T) {
+	task := Task{
+		Description: "user description for task named 'test'",
+		Name:        "test",
+		Source:      "namespace/consul-terraform-sync/consul//modules/test",
+		Version:     "0.0.0",
+	}
+
+	testCases := []struct {
+		Name   string
+		Func   func(io.Writer, string, *RootModuleInputData) error
+		Golden string
+		Input  RootModuleInputData
+	}{
+		{
+			Name:   "main.tf",
+			Func:   newMainTF,
+			Golden: "testdata/main.tf",
+			Input: RootModuleInputData{
+				Backend: map[string]interface{}{
+					"consul": map[string]interface{}{
+						"scheme": "https",
+						"path":   "consul-terraform-sync/terraform",
+					},
+				},
+				Providers: []hcltmpl.NamedBlock{hcltmpl.NewNamedBlock(
+					map[string]interface{}{
+						"testProvider": map[string]interface{}{
+							"alias": "tp",
+							"obj": map[string]interface{}{
+								"username": "name",
+								"id":       "123",
+							},
+							"attr":  "value",
+							"count": 10,
+						},
+					})},
+				ProviderInfo: map[string]interface{}{
+					"testProvider": map[string]interface{}{
+						"version": "1.0.0",
+						"source":  "namespace/testProvider",
+					},
+				},
+				Task: task,
+				Variables: hcltmpl.Variables{
+					"one":       cty.NumberIntVal(1),
+					"bool_true": cty.BoolVal(true),
+				},
+			},
+		}, {
+			Name:   "variables.tf",
+			Func:   newVariablesTF,
+			Golden: "testdata/variables.tf",
+			Input: RootModuleInputData{
+				TerraformVersion: goVersion.Must(goVersion.NewSemver("0.99.9")),
+				Providers: []hcltmpl.NamedBlock{hcltmpl.NewNamedBlock(
+					map[string]interface{}{
+						"testProvider": map[string]interface{}{
+							"alias": "tp",
+							"obj": map[string]interface{}{
+								"username": "name",
+								"id":       "123",
+							},
+							"attr":  "value",
+							"count": 10,
+						},
+					})},
+				Task: task,
+			},
+		}, {
+			Name:   "terraform.tfvars.tmpl",
+			Func:   newTFVarsTmpl,
+			Golden: "testdata/terraform.tfvars.tmpl",
+			Input: RootModuleInputData{
+				Providers: []hcltmpl.NamedBlock{hcltmpl.NewNamedBlock(
+					map[string]interface{}{
+						"testProvider": map[string]interface{}{
+							"alias": "tp",
+							"attr":  "value",
+							"count": 10,
+						},
+					})},
+				Services: []Service{
+					{
+						Name:        "web",
+						Description: "web service",
+					}, {
+						Name:        "api",
+						Namespace:   "",
+						Datacenter:  "dc1",
+						Description: "api service for web",
+						Tag:         "tag",
+					},
+				},
+				Task: task,
+			},
+		}, {
+			Name:   "providers.tfvars",
+			Func:   newProvidersTFVars,
+			Golden: "testdata/providers.tfvars",
+			Input: RootModuleInputData{
+				Providers: []hcltmpl.NamedBlock{hcltmpl.NewNamedBlock(
+					map[string]interface{}{
+						"testProvider": map[string]interface{}{
+							"alias": "tp",
+							"attr":  "value",
+							"count": 10,
+						},
+					})},
+				Task: task,
+			},
+		}, {
+			Name:   "variables.module.tf",
+			Func:   newModuleVariablesTF,
+			Golden: "testdata/variables.module.tf",
+			Input: RootModuleInputData{
+				Variables: hcltmpl.Variables{
+					"num": cty.NumberIntVal(10),
+					"b":   cty.BoolVal(true),
+					"key": cty.StringVal("some_key"),
+					"obj": cty.ObjectVal(map[string]cty.Value{
+						"argStr": cty.StringVal("value"),
+						"argNum": cty.NumberFloatVal(-0.52),
+						"argList": cty.ListVal([]cty.Value{
+							cty.StringVal("a"),
+							cty.StringVal("b"),
+							cty.StringVal("c"),
+						}),
+						"argMap": cty.MapValEmpty(cty.Bool),
+					}),
+					"l": cty.ListVal([]cty.Value{
+						cty.NumberIntVal(4),
+						cty.NumberIntVal(0),
+					}),
+					"tup": cty.TupleVal([]cty.Value{
+						cty.StringVal("abc"),
+						cty.NumberIntVal(123),
+						cty.BoolVal(false),
+					}),
+				},
+				Task: task,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			input := tc.Input
+			input.Init()
+			b := new(bytes.Buffer)
+			err := tc.Func(b, tc.Name, &input)
+			require.NoError(t, err)
+			checkGoldenFile(t, tc.Golden, b.Bytes())
+		})
+	}
+}
+
+func checkGoldenFile(t *testing.T, goldenFile string, actual []byte) {
+	// update golden files if necessary
+	if *update {
+		if err := ioutil.WriteFile(goldenFile, actual, 0644); err != nil {
+			require.NoError(t, err)
+		}
+	}
+
+	gld, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, string(gld), string(actual))
+}
---
+++ consul-terraform-sync/templates/tftmpl/tfvars.go
@@ -0,0 +1,182 @@
+package tftmpl
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/hashicorp/hcat/dep"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/zclconf/go-cty/cty"
+)
+
+type healthService struct {
+	// Consul service information
+	ID        string            `hcl:"id"`
+	Name      string            `hcl:"name"`
+	Kind      string            `hcl:"kind"`
+	Address   string            `hcl:"address"`
+	Port      int               `hcl:"port"`
+	Meta      map[string]string `hcl:"meta"`
+	Tags      []string          `hcl:"tags"`
+	Namespace cty.Value         `hcl:"namespace"`
+	Status    string            `hcl:"status"`
+
+	// Consul node information for a service
+	Node                string            `hcl:"node"`
+	NodeID              string            `hcl:"node_id"`
+	NodeAddress         string            `hcl:"node_address"`
+	NodeDatacenter      string            `hcl:"node_datacenter"`
+	NodeTaggedAddresses map[string]string `hcl:"node_tagged_addresses"`
+	NodeMeta            map[string]string `hcl:"node_meta"`
+
+	// Added CTS information for a service
+	CTSUserDefinedMeta map[string]string `hcl:"cts_user_defined_meta"`
+}
+
+func newHealthService(s *dep.HealthService, ctsUserDefinedMeta map[string]string) healthService {
+	if s == nil {
+		return healthService{}
+	}
+
+	// Namespace is null-able
+	var namespace cty.Value
+	if s.Namespace != "" {
+		namespace = cty.StringVal(s.Namespace)
+	} else {
+		namespace = cty.NullVal(cty.String)
+	}
+
+	// Default to empty list instead of null
+	tags := []string{}
+	if s.Tags != nil {
+		tags = s.Tags
+	}
+
+	return healthService{
+		ID:        s.ID,
+		Name:      s.Name,
+		Kind:      s.Kind,
+		Address:   s.Address,
+		Port:      s.Port,
+		Meta:      nonNullMap(s.ServiceMeta),
+		Tags:      tags,
+		Namespace: namespace,
+		Status:    s.Status,
+
+		Node:                s.Node,
+		NodeID:              s.NodeID,
+		NodeAddress:         s.NodeAddress,
+		NodeDatacenter:      s.NodeDatacenter,
+		NodeTaggedAddresses: nonNullMap(s.NodeTaggedAddresses),
+		NodeMeta:            nonNullMap(s.NodeMeta),
+
+		CTSUserDefinedMeta: nonNullMap(ctsUserDefinedMeta),
+	}
+}
+
+// newTFVarsTmpl writes the hcat services template to a .tfvars file. This is used
+// by hcat for monitoring service changes from Consul.
+func newTFVarsTmpl(w io.Writer, filename string, input *RootModuleInputData) error {
+	err := writePreamble(w, input.Task, filename)
+	if err != nil {
+		return err
+	}
+
+	hclFile := hclwrite.NewEmptyFile()
+	body := hclFile.Body()
+	appendRawServiceTemplateValues(body, input.Services)
+
+	_, err = hclFile.WriteTo(w)
+	return err
+}
+
+// newProvidersTFVars writes input variables for configured Terraform providers.
+func newProvidersTFVars(w io.Writer, filename string, input *RootModuleInputData) error {
+	err := writePreamble(w, input.Task, filename)
+	if err != nil {
+		return err
+	}
+
+	hclFile := hclwrite.NewEmptyFile()
+	body := hclFile.Body()
+
+	lastIdx := len(input.Providers) - 1
+	for i, p := range input.Providers {
+		obj := p.ObjectVal()
+		body.SetAttributeValue(p.Name, *obj)
+		if i != lastIdx {
+			body.AppendNewline()
+		}
+	}
+
+	_, err = hclFile.WriteTo(w)
+	return err
+}
+
+// appendRawServiceTemplateValues appends raw lines representing blocks that
+// assign value to the services variable `VariableServices` with `hcat` template
+// syntax for dynamic rendering of Consul dependency values.
+//
+// services = {
+//   <service>: {
+//	   {{ <template syntax> }}
+//   }
+// }
+func appendRawServiceTemplateValues(body *hclwrite.Body, services []Service) {
+	if len(services) == 0 {
+		return
+	}
+
+	tokens := make([]*hclwrite.Token, 0, len(services)+2)
+	tokens = append(tokens, &hclwrite.Token{
+		Type:  hclsyntax.TokenOBrace,
+		Bytes: []byte("{"),
+	})
+	lastIdx := len(services) - 1
+	for i, s := range services {
+		rawService := fmt.Sprintf(baseAddressStr, s.hcatQuery())
+
+		if i == lastIdx {
+			rawService += "\n}"
+		} else {
+			nextS := services[i+1]
+			rawComma := fmt.Sprintf(baseCommaStr, s.hcatQuery(), nextS.hcatQuery())
+			rawService += rawComma
+		}
+
+		token := hclwrite.Token{
+			Type:  hclsyntax.TokenNil,
+			Bytes: []byte(rawService),
+		}
+		tokens = append(tokens, &token)
+	}
+	body.SetAttributeRaw("services", tokens)
+}
+
+func nonNullMap(m map[string]string) map[string]string {
+	if m == nil {
+		return map[string]string{}
+	}
+
+	return m
+}
+
+// baseAddressStr is the raw template following hcat syntax for addresses of
+// Consul services.
+const baseAddressStr = `
+{{- with $srv := service %s }}
+  {{- $last := len $srv | subtract 1}}
+  {{- range $i, $s := $srv}}
+  "{{ joinStrings "." .ID .Node .Namespace .NodeDatacenter }}" : {
+{{ HCLService $s | indent 4 }}
+  } {{- if (ne $i $last)}},{{end}}
+  {{- end}}
+{{- end}}`
+
+// baseCommaStr is the raw template following hcat syntax for the comma between
+// different Consul services. Rendering a comma requires there to be an instance
+// of the service before and after the comma.
+const baseCommaStr = `{{- with $beforeSrv := service %s }}
+  {{- with $afterSrv := service %s }},{{end}}
+{{- end}}`
