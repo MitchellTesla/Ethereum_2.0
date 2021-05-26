11970c94393e47a8f05e83e24f3843775581da1d
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-26 03:27:59 -0400

    Update 🔄 .secrets/lib/web3x/web3x/src/ens

---
+++ .secrets/lib/web3x/web3x/src/ens/config.ts
@@ -0,0 +1,24 @@
+/*
+  This file is part of web3x.
+
+  web3x is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  web3x is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with web3x.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+export const config = {
+  addresses: {
+    main: '0x314159265dD8dbb310642f98f50C066173C1259b',
+    ropsten: '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+    rinkeby: '0xe7410170f87102df0055eb195163a03b7f2bff4a',
+  },
+};
