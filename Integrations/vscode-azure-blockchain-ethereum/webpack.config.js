79756df031b6d08318eb0cd2aeb63a17d6b086c2
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-28 23:13:50 -0400

    ♻️ Update from release v0.1.13

---
+++ Integrations/vscode-azure-blockchain-ethereum/webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const config = {
+  target: 'node',
+  entry: {
+    'extension': './src/extension.ts',
+    'debugger': './src/debugger.ts',
+  },
+  output: {
+    path: path.join(__dirname, 'out', 'src'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  devtool: false,
+  optimization: {
+    minimize: true
+  },
+  externals: function (context, request, callback) {
+    // don't pack any require/import that requests any file/module with
+    // `"mscorlib"` in it
+    if (/mscorlib/i.test(request)) {
+      return callback(null, 'commonjs ' + request);
+    } else if (/Nethereum/i.test(request)) {
+      return callback(null, 'commonjs ' + request);
+    // don't pack any require/import that requests `"vscode"`
+    } else if (/^vscode$/.test(request)) {
+      return callback(null, 'commonjs ' + request);
+    } else if (/^electron$/.test(request)) {
+      return callback(null, 'require ("' + request + '")');
+    }
+    callback();
+  },
+  resolve: {
+    // .json is added to prevent import error from /node_modules/got/index.js
+    extensions: ['.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: './src/Generators/mscorlib.js', to: './' },
+      { from: './src/Generators/Nethereum.Generators.DuoCode.js', to: './' },
+      { from: './src/debugAdapter/web3ProviderResolver.js', to: './' },
+      { from: './src/helpers/checkTruffleConfigTemplate.js', to: './' },
+    ]),
+    new webpack.DefinePlugin({
+      IS_BUNDLE_TIME: true,
+    })
+  ],
+  node: {
+    __dirname: false,
+  }
+};
+module.exports = config;
