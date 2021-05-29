23a47c6288a44a5ad263698afcd0f411ce0366d8
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-29 14:56:32 -0400

    ♻️ Update from release v1.6.0

---
+++ Integrations/vscode-azure-blockchain-ethereum/src/services/HttpService.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import * as requestPromise from 'request-promise';
+import { Constants } from '../Constants';
+import { Telemetry } from '../TelemetryClient';
+
+const requestTimeout = 10000;
+
+export namespace HttpService {
+  export async function sendRPCRequest(host: string, methodName: string, parameters?: string[])
+: Promise<{ result?: any, error?: any } | undefined> {
+    const address = hasProtocol(host) ? host : `${Constants.networkProtocols.http}${host}`;
+    return requestPromise.post(
+      address,
+      {
+        body: {
+          id: 1,
+          jsonrpc: '2.0',
+          method: methodName,
+          params: parameters || [],
+        },
+        json: true,
+        timeout: requestTimeout,
+      })
+      .catch((_errorMessage) => {
+        Telemetry.sendException(new Error(`HttpService.sendRPCRequest has done with error for method: ${methodName}`));
+
+        return undefined;
+      });
+  }
+
+  function hasProtocol(host: string): boolean {
+    return host.indexOf(Constants.networkProtocols.http) === 0 || host.indexOf(Constants.networkProtocols.https) === 0;
+  }
+}
---
+++ Integrations/vscode-azure-blockchain-ethereum/src/services/MnemonicRepository.ts
@@ -0,0 +1,43 @@
+import * as fs from 'fs-extra';
+import { Memento } from 'vscode';
+import { Constants } from '../Constants';
+
+class ExtensionMnemonicRepository {
+  private globalState?: Memento;
+
+  public initialize(globalState: Memento): void {
+    this.globalState = globalState;
+  }
+
+  public getMnemonic(filePath: string): string {
+    return fs.readFileSync(filePath).toString().trim();
+  }
+
+  public getAllMnemonicPaths(): string[] {
+    if (this.globalState) {
+      return this.globalState.get(Constants.globalStateKeys.mnemonicStorageKey, []) as string[];
+    }
+
+    return [];
+  }
+
+  public getExistedMnemonicPaths(): string[] {
+    return this.getAllMnemonicPaths().filter((path) => fs.existsSync(path));
+  }
+
+  public saveMnemonicPath(filePath: string): void {
+    if (this.globalState) {
+      const storage = this.globalState.get(Constants.globalStateKeys.mnemonicStorageKey) as string[] || [];
+      this.globalState.update(Constants.globalStateKeys.mnemonicStorageKey, [...storage, filePath]);
+    }
+  }
+
+  public MaskMnemonic(mnemonic: string) {
+    return mnemonic
+      ? `${mnemonic.slice(0, 3)} ... ${mnemonic.slice(-3)}`
+      : Constants.placeholders.emptyLineText;
+  }
+}
+
+// tslint:disable-next-line:variable-name
+export const MnemonicRepository = new ExtensionMnemonicRepository();
