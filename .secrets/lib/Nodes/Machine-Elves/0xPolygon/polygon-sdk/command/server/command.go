4a8748dffe89c5ff646917a0c7eacda65e852121
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-06-03 12:56:48 -0400

    Added the usage description for the NAT cli argument.

---
+++ .secrets/lib/Nodes/Machine-Elves/0xPolygon/polygon-sdk/command/server/command.go
@@ -0,0 +1,146 @@
+package server
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/0xPolygon/minimal/minimal"
+	"github.com/hashicorp/go-hclog"
+	"github.com/mitchellh/cli"
+)
+
+// Command is the command to start the sever
+type Command struct {
+	UI cli.Ui
+}
+
+// Help implements the cli.Command interface
+func (c *Command) Help() string {
+	return ""
+}
+
+// Synopsis implements the cli.Command interface
+func (c *Command) Synopsis() string {
+	return ""
+}
+
+// Run implements the cli.Command interface
+func (c *Command) Run(args []string) int {
+	conf, err := readConfig(args)
+	if err != nil {
+		c.UI.Error(err.Error())
+
+		return 1
+	}
+
+	config, err := conf.BuildConfig()
+	if err != nil {
+		c.UI.Error(err.Error())
+
+		return 1
+	}
+
+	logger := hclog.New(&hclog.LoggerOptions{
+		Name:  "polygon",
+		Level: hclog.LevelFromString("debug"),
+	})
+
+	server, err := minimal.NewServer(logger, config)
+	if err != nil {
+		c.UI.Error(err.Error())
+
+		return 1
+	}
+
+	if conf.Join != "" {
+		// make a non-blocking join request
+		server.Join(conf.Join, 0)
+	}
+
+	return c.handleSignals(server.Close)
+}
+
+// handleSignals listens for any client related signals, and closes the client accordingly
+func (c *Command) handleSignals(closeFn func()) int {
+	signalCh := make(chan os.Signal, 4)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+
+	var sig os.Signal
+	select {
+	case sig = <-signalCh:
+	}
+
+	c.UI.Output(fmt.Sprintf("Caught signal: %v", sig))
+	c.UI.Output("Gracefully shutting down agent...")
+
+	// Call the Minimal server close callback
+	gracefulCh := make(chan struct{})
+	go func() {
+		if closeFn != nil {
+			closeFn()
+		}
+		close(gracefulCh)
+	}()
+
+	select {
+	case <-signalCh:
+		return 1
+	case <-time.After(5 * time.Second):
+		return 1
+	case <-gracefulCh:
+		return 0
+	}
+}
+
+func readConfig(args []string) (*Config, error) {
+	config := defaultConfig()
+
+	cliConfig := &Config{
+		Network: &Network{},
+	}
+
+	flags := flag.NewFlagSet("server", flag.ContinueOnError)
+	flags.Usage = func() {}
+
+	var configFile string
+	flags.StringVar(&cliConfig.LogLevel, "log-level", "", "")
+	flags.BoolVar(&cliConfig.Seal, "seal", false, "")
+	flags.StringVar(&configFile, "config", "", "")
+	flags.StringVar(&cliConfig.Chain, "chain", "", "")
+	flags.StringVar(&cliConfig.DataDir, "data-dir", "", "")
+	flags.StringVar(&cliConfig.GRPCAddr, "grpc", "", "")
+	flags.StringVar(&cliConfig.JSONRPCAddr, "jsonrpc", "", "")
+	flags.StringVar(&cliConfig.Join, "join", "", "")
+	flags.StringVar(&cliConfig.Network.Addr, "libp2p", "", "")
+	flags.StringVar(&cliConfig.Network.NatAddr, "nat", "", "the external IP address without port, as can be seen by peers")
+	flags.BoolVar(&cliConfig.Network.NoDiscover, "no-discover", false, "")
+	flags.Uint64Var(&cliConfig.Network.MaxPeers, "max-peers", 0, "")
+	flags.BoolVar(&cliConfig.Dev, "dev", false, "")
+	flags.Uint64Var(&cliConfig.DevInterval, "dev-interval", 0, "")
+
+	if err := flags.Parse(args); err != nil {
+		return nil, err
+	}
+
+	if configFile != "" {
+		// A config file has been passed in, parse it
+		diskConfigFile, err := readConfigFile(configFile)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := config.mergeConfigWith(diskConfigFile); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := config.mergeConfigWith(cliConfig); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
