8908e77ada4da820933ba1c806357937e9e28e7d
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-06-03 12:31:46 -0400

    Feature libp2p + new protocol sync
    7

---
+++ .secrets/lib/Nodes/Machine-Elves/0xPolygon/polygon-sdk/blockchain/storage/leveldb/leveldb_test.go
@@ -0,0 +1,31 @@
+package leveldb
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/0xPolygon/minimal/blockchain/storage"
+	"github.com/hashicorp/go-hclog"
+)
+
+func newStorage(t *testing.T) (storage.Storage, func()) {
+	path, err := ioutil.TempDir("/tmp", "minimal_storage")
+	if err != nil {
+		t.Fatal(err)
+	}
+	s, err := NewLevelDBStorage(path, hclog.NewNullLogger())
+	if err != nil {
+		t.Fatal(err)
+	}
+	close := func() {
+		if err := os.RemoveAll(path); err != nil {
+			t.Fatal(err)
+		}
+	}
+	return s, close
+}
+
+func TestStorage(t *testing.T) {
+	storage.TestStorage(t, newStorage)
+}
