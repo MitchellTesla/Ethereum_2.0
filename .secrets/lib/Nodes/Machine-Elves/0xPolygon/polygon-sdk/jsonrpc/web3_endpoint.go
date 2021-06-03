3649068364637076723c70739b66b595dbe8f347
jmitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-06-03 15:12:53 -0400

    feature/code-cleanup
    37

---
+++ .secrets/lib/Nodes/Machine-Elves/0xPolygon/polygon-sdk/jsonrpc/web3_endpoint.go
@@ -0,0 +1,30 @@
+package jsonrpc
+
+import (
+	"fmt"
+
+	"github.com/0xPolygon/minimal/helper/hex"
+	"github.com/0xPolygon/minimal/helper/keccak"
+	"github.com/0xPolygon/minimal/version"
+)
+
+// Web3 is the web3 jsonrpc endpoint
+type Web3 struct {
+	d *Dispatcher
+}
+
+// ClientVersion returns the version of the web3 client (web3_clientVersion)
+func (w *Web3) ClientVersion() (interface{}, error) {
+	return fmt.Sprintf("polygon-sdk [%s]", version.GetVersion()), nil
+}
+
+// Sha3 returns Keccak-256 (not the standardized SHA3-256) of the given data
+func (w *Web3) Sha3(val string) (interface{}, error) {
+	v, err := hex.DecodeHex(val)
+	if err != nil {
+		return nil, err
+	}
+	dst := keccak.Keccak256(nil, v)
+
+	return hex.EncodeToHex(dst), nil
+}
